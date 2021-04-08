5bcb2fb558a02e75dc8007aba8c5b96f2a3ae7b3
jogo-hm <josegonzahm@gmail.com>
2021-04-08 01:16:15 -0600

    Carga scripts

---
+++ database/PL_script.sql
@@ -0,0 +1,114 @@
+
+--PRÀCTICA
+CREATE OR REPLACE FUNCTION getRegistros() 
+RETURNS TABLE (
+				id_registro INTEGER,
+				fecha_hora TIMESTAMP,
+				cantidad INTEGER,
+				monto NUMERIC,
+				empleado VARCHAR,
+				producto VARCHAR
+			  )
+LANGUAGE plpgsql
+AS $$
+BEGIN
+	RETURN QUERY
+	SELECT R.id_registro,R.fecha_hora,R.cantidad,R.monto,E.nombre AS empleado,P.nombre AS producto 
+	FROM REGISTRO R
+	JOIN EMPLEADO E ON EMPLEADO_id_empleado=id_empleado 
+	JOIN PRODUCTO P ON PRODUCTO_id_producto=id_producto;
+
+END;
+$$
+
+CREATE OR REPLACE FUNCTION getHistorial()
+RETURNS TABLE (
+				id_historial_precio INTEGER,
+				fecha_hora TIMESTAMP,
+				cantidad NUMERIC(5,2),
+				aumento_disminucion BOOLEAN,
+				producto VARCHAR
+			  )
+LANGUAGE plpgsql
+AS 
+$$
+	BEGIN
+		RETURN QUERY
+			SELECT H.id_historial_precio,H.fecha_hora,H.aumento_disminucion,P.nombre AS producto
+			FROM historial_precio H
+			JOIN PRODUCTO P ON PRODUCTO_id_producto=id_producto;
+	END;
+$$
+
+--HISTORIAL
+CREATE OR REPLACE FUNCTION cambiosPrecio()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS 
+$$
+	DECLARE
+		diferencia NUMERIC(5,2);
+	BEGIN
+		diferencia:= NEW.precio - OLD.precio;
+				
+		IF diferencia > 0 THEN
+			INSERT INTO HISTORIAL_PRECIO(
+											fecha_hora,
+										 	cantidad,
+										 	aumento_disminucion,
+										 	producto_id_producto
+										)
+			VALUES(CURRENT_TIMESTAMP,diferencia,TRUE,NEW.id_producto);
+		ELSE
+			INSERT INTO HISTORIAL_PRECIO(
+											fecha_hora,
+										 	cantidad,
+										 	aumento_disminucion,
+										 	producto_id_producto
+										)
+			VALUES(CURRENT_TIMESTAMP,diferencia,FALSE,OLD.id_producto);
+		END IF;
+		
+		RETURN NEW;
+END;$$
+
+DROP TRIGGER actualizacionPrecio
+ON PRODUCTO;
+
+CREATE TRIGGER actualizacionPrecio
+AFTER UPDATE
+ON PRODUCTO
+FOR EACH ROW
+EXECUTE PROCEDURE cambiosPrecio();
+
+
+--INVENTARIAR
+CREATE OR REPLACE FUNCTION verificarInventario()
+RETURNS TRIGGER
+LANGUAGE plpgsql
+AS
+$$
+	DECLARE
+		stock INTEGER;
+		diferencia INTEGER;
+	BEGIN
+		stock:=(SELECT P.stock FROM PRODUCTO P WHERE NEW.PRODUCTO_id_producto=id_producto);
+		
+		diferencia:=stock-NEW.cantidad;
+		
+		IF diferencia >= 0 THEN
+			UPDATE PRODUCTO SET stock=diferencia;
+		ELSE
+			NEW.id_producto:=-1;
+		END IF;
+				RETURN NEW;
+
+	END;
+$$
+
+CREATE TRIGGER gestionarStock
+BEFORE INSERT 
+ON REGISTRO
+FOR EACH ROW
+EXECUTE PROCEDURE verificarInventario();
+
---
+++ database/script.sql
@@ -0,0 +1,80 @@
+CREATE TABLE EMPLEADO (
+    id_empleado  SERIAL,
+    nombre       VARCHAR(30) NOT NULL,
+    salario      NUMERIC(10, 2) NOT NULL
+);
+
+ALTER TABLE EMPLEADO ADD CONSTRAINT EMPLEADO_pk PRIMARY KEY ( id_empleado );
+
+CREATE TABLE HISTORIAL_PRECIO (
+    id_historial_precio   SERIAL,
+    fecha_hora            TIMESTAMP NOT NULL,
+    cantidad              NUMERIC(5,2) NOT NULL,
+    aumento_disminucion   BOOLEAN NOT NULL,
+    producto_id_producto  INTEGER NOT NULL
+);
+
+ALTER TABLE HISTORIAL_PRECIO ADD CONSTRAINT HISTORIAL_PRECIO_pk PRIMARY KEY ( id_historial_precio );
+
+CREATE TABLE producto (
+    id_producto  SERIAL,
+    nombre       VARCHAR(30) NOT NULL,
+    descripcion  VARCHAR(100) NOT NULL,
+    stock        INTEGER NOT NULL,
+    precio       NUMERIC(5, 2) NOT NULL
+);
+
+ALTER TABLE PRODUCTO ADD CONSTRAINT PRODUCTO_pk PRIMARY KEY ( id_producto );
+
+ALTER TABLE PRODUCTO ADD CONSTRAINT PRODUCTO_nombre_un UNIQUE ( nombre );
+
+CREATE TABLE REGISTRO (
+    id_registro           SERIAL,
+    cantidad              INTEGER NOT NULL,
+    monto                 NUMERIC(5,2) NOT NULL,
+    fecha_hora            TIMESTAMP NOT NULL,
+    PRODUCTO_id_producto  INTEGER NOT NULL,
+    EMPLEADO_id_empleado  INTEGER NOT NULL
+);
+
+ALTER TABLE REGISTRO ADD CONSTRAINT REGISTRO_pk PRIMARY KEY ( id_registro );
+
+ALTER TABLE HISTORIAL_PRECIO
+    ADD CONSTRAINT HISTORIAL_PRECIO_PRODUCTO_fk FOREIGN KEY ( PRODUCTO_id_producto )
+        REFERENCES PRODUCTO ( id_producto ) ON DELETE CASCADE;
+
+ALTER TABLE REGISTRO
+    ADD CONSTRAINT REGISTRO_EMPLEADO_fk FOREIGN KEY ( EMPLEADO_id_empleado )
+        REFERENCES EMPLEADO ( id_empleado ) ON DELETE CASCADE;
+
+ALTER TABLE REGISTRO
+    ADD CONSTRAINT REGISTRO_PRODUCTO_fk FOREIGN KEY ( PRODUCTO_id_producto )
+        REFERENCES PRODUCTO ( id_producto ) ON DELETE CASCADE;
+
+
+
+--Insertar empleados
+INSERT INTO EMPLEADO(nombre,salario) VALUES('Eren Jeager',1000.50);
+INSERT INTO EMPLEADO(nombre,salario) VALUES('Mikasa Akerman',1500.75);
+INSERT INTO EMPLEADO(nombre,salario) VALUES('Armin Arlet',2500);
+INSERT INTO EMPLEADO(nombre,salario) VALUES('Levi Ackerman',3000);
+INSERT INTO EMPLEADO(nombre,salario) VALUES('Erwin Smith',4000);
+INSERT INTO EMPLEADO(nombre,salario) VALUES('Sasha Blouse',2000);
+INSERT INTO EMPLEADO(nombre,salario) VALUES('Reiner Braun',2000);
+
+
+--Insertar productos
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Cloro magia blanca','Cloro, presentación de 200ml',5.25,10);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Azistin manzana verde','Desinfectante presentación de un galón',62,5);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Papel higienico','Unidad de papel higiendico',5,20);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Mascarilla N95','Mascarilla desechable',19.5,15);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Antibacterial Blumen Fresh','Alcohol en gel',24.99,3);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Servilletas SULI BLANCA','Paquete de servilletas 500 unidades',3.5,9);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Jugo de la granja','Jugo de naranja con pulpa',4,15);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Café de jarrillita instantaneo','Sobre de cafe instantanio 8GR',2,17);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Aua GREAT VALUE','Botella de agua purificada 1LT',3,15);
+INSERT INTO PRODUCTO(nombre,descripcion,stock,precio) VALUES('Sopa Ramen.',' LAKY MEN CUP Pollo. 75 Gramos',5,10);
+
+--Insertar registros
+INSERT INTO REGISTRO(cantidad,monto,fecha_hora,PRODUCTO_id_producto,EMPLEADO_id_empleado) VALUES(2,100,NOW(),1,1);
+INSERT INTO REGISTRO(cantidad,monto,fecha_hora,PRODUCTO_id_producto,EMPLEADO_id_empleado) VALUES(2,100,CURRENT_TIMESTAMP,2,2);
